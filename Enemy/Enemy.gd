a6c745e467f526f6a809b6e091ad5a6e0050c0ba
carter-null <70243434+carter-null@users.noreply.github.com>
2020-09-22 17:50:13 -0400

    Done

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var HUD = get_node("/root/Game/HUD")
+
 export var speed = Vector2(0,3)
 export var health = 100
 export var points = 10
@@ -8,13 +10,14 @@ export var damage = 50
 onready var Explosion = load("res://Explosion/Explosion.tscn")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	position += speed
 
 	if position.y > get_viewport().size.y + 100:
 		queue_free()
 
 func die():
+	HUD.update_score(points)
 	var explosion = Explosion.instance()
 	explosion.position = position
 	get_node("/root/Game/Explosions").add_child(explosion)
